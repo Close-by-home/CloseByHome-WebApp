e2a820006d13967c8682670938a729cdb9e9df9a
Leandro de Sales Cotrim <leandro.cotrim@bandtec.com.br>
2022-10-13 20:49:33 -0300

    Adicionando movimento suave no scroll dos links

--- src/components/componentsPaginas/navbar/Navbar.jsx
+++ src/components/componentsPaginas/navbar/Navbar.jsx
@@ -1,26 +1,45 @@
-import { Link } from 'react-router-dom'
+import { Link as LinkReact } from 'react-router-dom'
 
 import Botao from '../../componentsReutilizacao/botaoVazio/Botao' 
 
 import style from './Style.module.css'
 import logo from '../../../assets/img/CloseByHomeLogo.png'
+import { Link,animateScroll as scroll }  from "react-scroll";
 
 const Navbar = () => {
+  console.log(window.window.pageYOffset);
+  
   return (
     <nav className={style.nav}>
       <div className={style.opcoes}>
         <img src={logo} alt=""/>
-        <ul>
-          <li>Inicio</li>
-          <li>Como Funciona?</li>
-          <li>Sobre Nós!</li>
-          <li>Entre em Contato</li>
-        </ul>
+        <div className={style.div_itens}>
+          <Link 
+            style={{ cursor: "pointer" }} activeClass="active" to="sec_inicio" spy={true} smooth={true} offset={-100} duration={500}
+          >
+            Inicio
+          </Link>
+          <Link 
+            style={{ cursor: "pointer" }} activeClass="active" to="sec_passoAPasso" spy={true} smooth={true} offset={-100} duration={500}
+          >
+            Como Funciona?
+          </Link>
+          <Link 
+            style={{ cursor: "pointer" }} activeClass="active" to="sec_sobre" spy={true} smooth={true} offset={-100} duration={500}
+          >
+            Sobre Nós!
+          </Link>
+          <Link 
+            style={{ cursor: "pointer" }} activeClass="active" to="sec_contato" spy={true} smooth={true} offset={-100} duration={500}
+          >
+            Entre em Contato
+          </Link>
+        </div>
       </div>
       <div className={style.link}>
-        <Link to="/login" style={{ textDecoration: "none" }}>
+        <LinkReact to="/login" style={{ textDecoration: "none" }}>
           <Botao text="Entrar" cor="azul"/>
-        </Link>
+        </LinkReact>
       </div>
     </nav>
   )
